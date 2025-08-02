74bd75e8604f644082715254603d2d1e4c1b502e
Swetony <swetonyancelmo@gmail.com>
2025-08-01 23:26:56 -0300

    funcao de adicionar nova tarefa

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import './TodoList.css';
 
 export default function TodoList() {
+
+  const [list, setList] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  // Adiciona a nova tarefa à lista
+  function addItem(form){
+    form.preventDefault();
+
+    if(!newItem){
+      return;
+    }
+
+    setList([...list, { text: newItem, isCompleted: false }]);
+    setNewItem("");
+    document.getElementById('input').focus();
+  }
+
   return(
     <div>
       <h1>Todo List</h1>
 
-      <form className='form-contain'>
-        <input 
+      <form onSubmit={addItem} className='form-contain'>
+        <input
+          id='input'
+          value={newItem}
+          onChange={(e) => { setNewItem(e.target.value) }}
           className='input-contain'
           type="text"
           placeholder='Digite uma tarefa'
